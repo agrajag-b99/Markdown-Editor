890eba8461fcbbe1865ed4d7c73def367634a043
agrajag <ansshu.sin@gmail.com>
2020-08-08 19:21:27 +0530

    changes

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -3,50 +3,81 @@ const AWS = require('aws-sdk');
 const BUCKET_NAME = '';         // Enter the name of s3 bucket that you want to publish to
 
 const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
 const uploadFile = (fileContent, fileName) => {
-    let folder='' //folder in which u want it stored eg: 'folder/subfolder/'
-    let fkey= folder + fileName + '.md';
-    // Setting up S3 upload parameters
-    const params = {
-        Bucket: BUCKET_NAME,
-        Key: fkey, // File name you want to save as in S3
-        Body: fileContent
-    };
+  let folder='' //folder in which u want it stored eg: 'folder/subfolder/' should end with '/'
+  let fkey= folder + fileName + '.md';
+  // Setting up S3 upload parameters
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: fkey, // File name you want to save as in S3
+    Body: fileContent
+  };
 
-    // Uploading files to the bucket
-    s3.upload(params, function(err, data) {
-        if (err) {
-            throw err;
-        }
-        console.log(`File uploaded successfully at ${data.Location}`);
-    });
+  // Uploading files to the bucket
+  s3.upload(params, function(err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log(`File uploaded successfully at ${data.Location}`);
+  });
 };
+
+function formatDate(date) {
+  var d = new Date(date),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) 
+    month = '0' + month;
+  if (day.length < 2) 
+    day = '0' + day;
+
+  return [year, month, day].join('-');
+}
 
 let {render} = require("mustache");
 let template = `---
 title: "{{titl}}"
 date: "{{dat}}"
+description: "{{desc}}"
+imgsrc: "/static/blogThumbnailPics/{{src}}.jpg"
 ---
-{{bod}}
-`
+{{content}}`
 
 export default (req, res) => {
   let data=[]
+  const Today = new Date();
+  const realDate = formatDate(Today)
   if(req.method==='POST'){
     res.statusCode = 200
-    const {title, date, bodi, mark} = req.body;
+    const timeKey = Today.getTime();
+
+    const {title, description, bodi, mark, image} = req.body;
+
+    var blogKey = title +' '+ timeKey;
+    blogKey = blogKey.replace(/\-/g,'').replace(/\?/g,'').replace(/\`/g,'').replace(/\~/g,'').replace(/\!/g,'').replace(/\@/g,'').replace(/\#/g,'').replace(/\$/g,'').replace(/\%/g,'').replace(/\^/g,'').replace(/\&/g,'').replace(/\*/g,'').replace(/\(/g,'').replace(/\)/g,'').replace(/\_/g,'').replace(/\=/g,'').replace(/\+/g,'').replace(/\{/g,'').replace(/\}/g,'').replace(/\[/g,'').replace(/\]/g,'').replace(/\|/g,'').replace(/\\/g,'').replace(/\//g,'').replace(/\>/g,'').replace(/\./g,'').replace(/\</g,'').replace(/\,/g,'').replace(/\:/g,'').replace(/\;/g,'').replace(/\'/g,'').replace(/\"/g,'').replace(/ /g,'-') // sorry for this but couldn't think of anything else to avoid illegal naming
+
     let blog = {
       titl:  title,
-      dat: date,
-      bod: mark
+      dat: realDate,
+      desc: description,
+      content: mark,
+      src: blogKey
     }
-    let newFile = render(template, blog);
-    uploadFile(newFile, blog.titl);
-    // fs.writeFileSync('./new.md', newFile);           // Use this command to generate the markdown file locally also
+
+    let newF = render(template, blog);
+    let newFile = newF.replace(/\&amp;/g, '&').replace(/\&#x60;/g, '`').replace(/\&#39;/g, "'").replace(/\&#x2F;/g, '/').replace(/\&quot;/g, '"').replace(/\&gt;/g, '>').replace(/\&lt;/g, '<').replace(/\&#x3D;/g, '=')
+    
+
+    
+    // uploadFile(newFile, blogKey);
+    fs.writeFileSync('./'+blogKey+'.md', newFile); // Use this command to generate the markdown file locally also
+    console.log(image); // Use this command to generate the markdown file locally also
     data.push(req.body)
     console.log(data.length);
     res.end()
--- pages/index.js
+++ pages/index.js
@@ -15,22 +15,25 @@ const Create=()=>{
     const [content, setContent] = useState('')
     const [mark,setmark]=useState('')
     const [arr,setarr]=useContext(Data)
+    const [pic, setPic]=useState(null)
     const router=useRouter()
     const summit=(e)=>{
         e.preventDefault()
         let data={
             title: e.target.title.value,
-            date: e.target.date.value,
+            description: e.target.description.value,
+            image: pic,
             bodi: content,
             mark: mark
         }
         Axios.post('http://localhost:3000/api/upload', data) //while deploying change api endpoint, for example, 'https://editor.higgle.io/api/upload' 
         .then(res=>console.log(res))
         e.target.title.value=''
-        e.target.date.value=''
+        e.target.description.value=''
         setContent('')
         setarr([data,...arr])
         router.push('/')
+        console.log(pic)
         
     }
         
@@ -48,14 +51,20 @@ const Create=()=>{
                     </div>
                     <span className='form-span'>Title:</span>
                     <input maxLength='100' name='title' required/>
-                    <span className='form-span'>Date:</span>
-                    <input maxLength='20' name='date' placeholder='YYYY-MM-DD' required/>
+                    <span className='form-span'>Description:</span>
+                    <input maxLength='200' name='description' required/>
+                    <span className='form-span'>Thumb Image:</span>
+                    <input type = "file" name='thumb' accept="image/*" required
+                        onChange={(e)=>{
+                            setPic(e);
+                        }}
+                    />
                     <span className='form-span'>Content:</span>
                     <div id='editor'>
                         <ReactQuill
                         onChange={(e)=>{
                             setContent(e)
-                            setmark(TurndownService({ headingStyle: 'atx' }).turndown(e))
+                            setmark(TurndownService({ headingStyle: 'atx' }).keep(['iframe', 'script']).turndown(e))
                             console.log(mark);
                         }}
                         modules={{
